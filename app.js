bce640ca0965968aa3d18f1d35bba1549a03956d
Amit Singh <amitdee97@gmail.com>
2025-05-04 02:41:40 +0530

    add some feature

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const dropdown = document.querySelector('.dropdown'); 
 const radioBtn = document.querySelector('#radioicon');
 
-radioBtn.onclick = function() {
+radioBtn.onclick = function(event) {
     dropdown.classList.toggle('open');
     const isOpen = dropdown.classList.contains('open');
 };
+
+document.addEventListener('click', function (event) {
+    if (!dropdown.contains(event.target) && !radioBtn.contains(event.target)) {
+        dropdown.classList.remove('open');
+    }
+});
